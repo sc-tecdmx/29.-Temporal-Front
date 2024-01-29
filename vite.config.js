180ae97cfd1466e9e3ec724c20c1bb80dfa11af6
Paola <paola.montg@gmail.com>
2024-01-29 11:54:45 -0600

    Se quita configuración vite.js

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   //base: 'https://jel.tecdmx.org.mx',
-    base: '/account/login',
-     server:{
-        https:true,
-        host: 'https://jel.tecdmx.org.mx',
-     },
+    // base: '/account/login',
+    //  server:{
+    //     https:true,
+    //     host: 'https://jel.tecdmx.org.mx',
+    //  },
   plugins: [
     vue(),
   ],

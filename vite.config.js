454c96d5cf65b518188f3182be8aa9ee77ba8cf9
Paola <paola.montg@gmail.com>
2024-01-29 11:42:30 -0600

    Configuración vite.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //base: 'https://jel.tecdmx.org.mx',
+    base: '/account/login',
+     server:{
+        https:true,
+        host: 'https://jel.tecdmx.org.mx',
+     },
   plugins: [
     vue(),
   ],
